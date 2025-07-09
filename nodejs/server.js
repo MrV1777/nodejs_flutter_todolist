71e7661458c4061895fecfeced50fcd87da18f7c
V <121571470+MrV1777@users.noreply.github.com>
2025-07-09 16:35:17 +0700

    server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -9,5 +9,5 @@ const taskRoutes = require('./routes/tasks');
 app.use('/tasks', taskRoutes);
 
 app.listen(3000, () => {
-  console.log('✅ Server running at http://localhost:3000');
+  console.log(' Server running at http://localhost:3000');
 });
